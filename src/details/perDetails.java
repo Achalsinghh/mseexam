fa67a84b12c63a9edbcff197892d2c88ca4bd3d8
AUTBLT410 <AUTBLT410@LT410-16.allianceblr.com>
2024-11-06 15:45:49 +0530

    pushing

---
+++ src/details/perDetails.java
@@ -0,0 +1,15 @@
+package details;
+
+public class perDetails {
+
+	public static void main(String[] args) {
+		String name = "Achal Singh";
+		int reg_no = 314;
+		int age = 314;
+		System.out.println("Name: " + name);
+		System.out.println("reg_no " + reg_no);
+		System.out.println("age: " + age);
+
+	}
+
+}
